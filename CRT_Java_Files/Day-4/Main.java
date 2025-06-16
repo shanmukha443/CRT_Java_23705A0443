037bc3a29a19650cf09ef31c1b2ca109a52ca0c3
shanmukha443 <shammurthy47@gmail.com>
2025-06-16 11:07:05 +0530

    Delete CRT_Java_Files directory

--- CRT_Java_Files/Day-4/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        Queues_Using_Linked_List q=new Queues_Using_Linked_List();
-        q.enqueue(30);
-        q.enqueue(20);
-        q.print();
-    }
-}
